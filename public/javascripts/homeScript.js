87bd09edef3e9b66bc703830965d67aef3788b87
Quintin <hendriks.quintin@gmail.com>
2017-01-25 01:40:46 +0100

    added backgrounds

--- public/javascripts/homeScript.js
+++ public/javascripts/homeScript.js
@@ -2,7 +2,7 @@
  * Created by Quintin on 25-1-2017.
  */
 $(function () {
-    var backgrounds = ["5863ce8f521afd1514391c67", "58678e2d5d7c10013cdc71a7", "5887e8566b92f01288f6db20", "58874e0785fb150011c8aba6", "5887f09da80f5d0011a8741a"];
+    var backgrounds = ["5863ce8f521afd1514391c67", "5887e8566b92f01288f6db20", "58874e0785fb150011c8aba6", "5887f09da80f5d0011a8741a", "5887f1a9c27d270011ec1ed0"];
     var random = Math.floor(Math.random() * backgrounds.length);
 
     function newBackground() {
