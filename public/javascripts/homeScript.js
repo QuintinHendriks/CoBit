5b9224b03e837ac4ffbf9e6fd2ac10a439437d2f
Quintin <hendriks.quintin@gmail.com>
2017-01-29 12:29:07 +0100

    added laoding icon for homepage and did some other stuff

--- public/javascripts/homeScript.js
+++ public/javascripts/homeScript.js
@@ -2,7 +2,7 @@
  * Created by Quintin on 25-1-2017.
  */
 $(function () {
-    var backgrounds = ["5863ce8f521afd1514391c67", "5887e8566b92f01288f6db20", "58874e0785fb150011c8aba6", "5887f09da80f5d0011a8741a", "5887f1a9c27d270011ec1ed0", "588b26c07b64520011f55c6f"];
+    var backgrounds = ["5863ce8f521afd1514391c67", "5887e8566b92f01288f6db20", "58874e0785fb150011c8aba6", "5887f09da80f5d0011a8741a", "5887f1a9c27d270011ec1ed0", "588b26c07b64520011f55c6f", "588d0aa506f2db0011b8844f"];
     var random = Math.floor(Math.random() * backgrounds.length);
 
     function newBackground() {
