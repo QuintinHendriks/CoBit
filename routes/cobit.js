0f62d648c3823a4964aed28c1a1b03e74071d668
Quintin <hendriks.quintin@gmail.com>
2017-02-12 23:51:32 +0100

    herro

--- routes/cobit.js
+++ routes/cobit.js
@@ -197,7 +197,7 @@ router.put("/:id/like", function (req, res) {
     var collection = db.get('coBits');
 
     collection.find({'_id': req.params.id}, {}, function(err, docs){
-        if(docs.likes.indexOf(req.body.liker) === -1){
+        if(docs.likes.indexOf(req.body.liker) === -1 || docs.likes === undefined){
             console.log(req.body.liker);
             collection.update({"_id": req.params.id}, {
                 $push: {"likes": req.body.liker}
