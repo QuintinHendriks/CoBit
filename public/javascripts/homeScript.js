77e12b4d1eaa4f69e243d65bb27edf159cf0be14
Quintin <hendriks.quintin@gmail.com>
2017-02-14 10:15:37 +0000

    fixed some stuff

--- public/javascripts/homeScript.js
+++ public/javascripts/homeScript.js
@@ -2,7 +2,7 @@
  * Created by Quintin on 25-1-2017.
  */
 $(function () {
-    var backgrounds = ["5887e8566b92f01288f6db20", "58874e0785fb150011c8aba6", "5887f09da80f5d0011a8741a", "5887f1a9c27d270011ec1ed0", "588b26c07b64520011f55c6f", "588d0aa506f2db0011b8844f", "588fc45465a63c001184ae5c", "58a0e3798f1ffb00116d5776", "58a1ffd3c04f242ee08f50aa", "58a20da7c04f242ee08f50ab"];
+    var backgrounds = ["5887e8566b92f01288f6db20", "5887f09da80f5d0011a8741a", "5887f1a9c27d270011ec1ed0", "588b26c07b64520011f55c6f", "588d0aa506f2db0011b8844f", "588fc45465a63c001184ae5c", "58a0e3798f1ffb00116d5776", "58a1ffd3c04f242ee08f50aa", "58a20da7c04f242ee08f50ab"];
     var random = Math.floor(Math.random() * backgrounds.length);
 
     function newBackground() {
@@ -30,7 +30,7 @@ $(function () {
             $("#coBitsWrapper").append("" +
                 "<div class='coBitPreview'>" +
                 "<iframe class='coBitPreviewFrame' src='https://co-bit.herokuapp.com/cobit/" + cobit_data[i].id + "/debug'></iframe>" +
-                "<a href='https://co-bit.herokuapp.com/cobit/" + cobit_data[i].id + "' class='toPreview'>"+cobit_data[i].title+"</a>" +
+                "<a href='https://co-bit.herokuapp.com/cobit/" + cobit_data[i].id + "' class='toPreview'>"+cobit_data[i].title+"</a></br>" +
                 "<p class='previewOwner'>Made by: "+cobit_data[i].owner+"</p>" +
                 "<p class='previewLikes'>"+likes+"</p>" +
                 "<i class='fa fa-heart previewLikeIcon' style='font-size: 15px;'></i>" +
